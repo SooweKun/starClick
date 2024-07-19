dfabe722d7b727fbf68fc7f81ab984f1db58aa46
iO <senpai9464@yandex.ru>
2024-07-20 04:57:08 +0500

    еще проще)

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,41 +6,36 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import {Link, useLocation} from '@tanstack/react-router';
-import {useState} from 'react';
 import {MenuIcon} from '../Icons/MenuIcon/menuIcon.jsx';
 
 const Menu = () => {
 	const {pathname} = useLocation();
-	const [isOpen, setIsOpen] = useState(false);
-
-	const handleMenuClick = () => setIsOpen(!isOpen);
 
 	return (
 		<DropdownMenu key={pathname}>
 			<DropdownMenuTrigger asChild>
-				<button onClick={handleMenuClick}><MenuIcon width={36} height={36} fill='black' /></button>
+				<button><MenuIcon width={36} height={36} fill='black' /></button>
 			</DropdownMenuTrigger>
 
-			{isOpen && (
-				<DropdownMenuContent className='w-56 mx-3'>
-					<DropdownMenuLabel>Menu</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<DropdownMenuGroup>
-						<DropdownMenuItem>
-							<Link to='/' className='w-56'>Home</Link>
-						</DropdownMenuItem>
-						<DropdownMenuItem>
+			<DropdownMenuContent className='w-56 mx-3'>
+				<DropdownMenuLabel>Menu</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<Link to='/' className='w-56'>Home</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
 						Accaunt
-						</DropdownMenuItem>
-						<DropdownMenuItem>
-							<Link to='/Test' className='w-56'>Mains</Link>
-						</DropdownMenuItem>
-						<DropdownMenuItem>
-							<Link to='/Market' className='w-56'>Market</Link>
-						</DropdownMenuItem>
-					</DropdownMenuGroup>
-				</DropdownMenuContent>
-			)}
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<Link to='/Test' className='w-56'>Mains</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<Link to='/Market' className='w-56'>Market</Link>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+
 		</DropdownMenu>
 	);
 };
